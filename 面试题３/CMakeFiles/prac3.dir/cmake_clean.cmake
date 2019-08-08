a51a822f352a2e8c29bac8375c223d6e2d9e4312
lixujun <18234044508@163.com>
2019-08-08 21:17:11 +0800

    third

---
+++ 面试题３/CMakeFiles/prac3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prac3.dir/main.cpp.o"
+  "prac3.pdb"
+  "prac3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prac3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
